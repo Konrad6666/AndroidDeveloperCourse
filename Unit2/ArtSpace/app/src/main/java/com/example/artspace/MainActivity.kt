27a0a3d6ca31a244ec71f5a10851ff0407731b52
Konrad <55025128+Konrad6666@users.noreply.github.com>
2024-02-25 15:41:55 +0100

    unit3 start, created scrollable list

--- Unit2/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ Unit2/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.artspace
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,11 +18,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -37,13 +47,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceTheme {
+                ArtSpaceApp()
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ArtSpaceApp()
-                }
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    ArtSpaceApp()
+//                }
             }
         }
     }
@@ -51,29 +62,55 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceApp(modifier: Modifier = Modifier) {
+    var number by rememberSaveable { mutableIntStateOf(1) }
+    var image = R.drawable.ic_launcher_foreground
+    var title= R.string.artwork_title_1
+    var artist= R.string.artwork_artist_and_year_1
+
+    when (number) {
+        1 -> {
+            image = R.drawable.ic_launcher_foreground
+            title = R.string.artwork_title_1
+            artist = R.string.artwork_artist_and_year_1
+        }
+        2 -> {
+            image = R.drawable.ic_launcher_background
+            title = R.string.artwork_title_2
+            artist = R.string.artwork_artist_and_year_2
+        }
+    }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState(0))
     ) {
         Spacer(modifier = Modifier.weight(1f))
-        Image(
-            painter = painterResource(R.drawable.ic_launcher_background),
-            contentDescription = null,
-            modifier = modifier
-                .shadow(5.dp, MaterialTheme.shapes.medium)
-                .size(300.dp)
-                .align(Alignment.CenterHorizontally)
-        )
+        MainImage(image)
         Spacer(modifier = Modifier.weight(1f))
-        TitleAndArtist()
-        PreviousNextButtons()
+        TitleAndArtist(title, artist)
+        PreviousNextButtons(
+            onClickPrevious = {
+                if (number < 2) {
+                    number = 2
+                } else {
+                    number--
+                }
+            },
+            onClickNext = {
+                if (number > 1) {
+                    number = 1
+                } else {
+                    number++
+                }
+            }
+        )
     }
 }
 
 @Composable
-fun TitleAndArtist(modifier: Modifier = Modifier) {
+fun TitleAndArtist(title: Int, artist:Int, modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
             .shadow(5.dp, MaterialTheme.shapes.small)
@@ -84,14 +121,14 @@ fun TitleAndArtist(modifier: Modifier = Modifier) {
                 .background(MaterialTheme.colorScheme.primaryContainer)
                 .padding(16.dp, 8.dp)
                 .fillMaxWidth(0.9f)
-            ) {
+        ) {
             Text(
-                text = stringResource(R.string.artwork_title_1),
+                text = stringResource(title),
                 fontWeight = FontWeight.Bold,
                 fontSize = 24.sp
             )
             Text(
-                text = stringResource(R.string.artwork_artist_and_year_1)
+                text = stringResource(artist),
             )
         }
     }
@@ -99,29 +136,47 @@ fun TitleAndArtist(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun PreviousNextButtons(modifier: Modifier = Modifier) {
+fun MainImage(
+    drawableResourceId: Int,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(drawableResourceId),
+        contentDescription = null,
+        modifier = modifier
+            .shadow(5.dp, MaterialTheme.shapes.medium)
+            .size(300.dp)
+    )
+}
+
+@Composable
+fun PreviousNextButtons(onClickPrevious: () -> Unit, onClickNext: () -> Unit, modifier: Modifier = Modifier) {
     Row(
         horizontalArrangement = Arrangement.Center,
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp, 10.dp)
     ) {
-        Button(
-            onClick = { },
-            modifier = modifier
-                .padding(16.dp)
-                .width(150.dp)
-        ) {
-            Text("Previous")
-        }
-        Button(
-            onClick = { },
-            modifier = modifier
-                .padding(16.dp)
-                .width(150.dp)
-        ) {
-            Text("Next")
-        }
+        Buttons(
+            onClick = onClickPrevious,
+            text = "Previous"
+        )
+        Buttons(
+            onClick = onClickNext,
+            text = "Next"
+        )
+    }
+}
+
+@Composable
+fun Buttons(onClick: () -> Unit, text: String, modifier: Modifier = Modifier) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .padding(16.dp)
+            .width(150.dp)
+    ) {
+        Text(text)
     }
 }
 
