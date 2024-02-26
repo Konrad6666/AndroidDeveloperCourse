1cc8aa43e12a0a69c48c08d46cd1757cf5cb09fd
Konrad <55025128+Konrad6666@users.noreply.github.com>
2024-02-26 16:54:42 +0100

    started Grid Practice

---
+++ Unit3/GridWithPhotos/app/src/main/java/com/example/gridwithphotos/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.example.gridwithphotos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gridwithphotos.model.Topic
+import com.example.gridwithphotos.ui.theme.GridWithPhotosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GridWithPhotosTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GridApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GridApp() {
+    GridCard(
+        topic = Topic(R.string.architecture, 58, R.drawable.architecture)
+    )
+}
+
+@Composable
+fun GridCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card {
+        Row {
+            Box{
+                Image(
+                    painter = painterResource(id = topic.imageResourceId),
+                    contentDescription = null,
+                    modifier = modifier
+                        .size(68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column {
+                Text(
+                    text = stringResource(id = topic.stringResourceId),
+                    style=MaterialTheme.typography.bodyMedium,
+                    modifier = modifier
+                        .padding(16.dp, 16.dp, 16.dp, 8.dp)
+                )
+                Row (verticalAlignment = Alignment.CenterVertically){
+                    Icon(
+                        painter = painterResource(id=R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start=16.dp)
+                    )
+                    Text(
+                        text = topic.label.toString(),
+                        style=MaterialTheme.typography.labelMedium,
+                        modifier = Modifier
+                            .padding(start=8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GridWithPhotosTheme {
+        GridApp()
+    }
+}
