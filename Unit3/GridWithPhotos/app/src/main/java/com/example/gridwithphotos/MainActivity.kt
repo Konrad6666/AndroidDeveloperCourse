214edb78de6ebfa1dadbac8b02939ef26aaaf2e8
Konrad <55025128+Konrad6666@users.noreply.github.com>
2024-02-27 15:05:13 +0100

    unit3 woof completed

--- Unit3/GridWithPhotos/app/src/main/java/com/example/gridwithphotos/MainActivity.kt
+++ Unit3/GridWithPhotos/app/src/main/java/com/example/gridwithphotos/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -25,6 +32,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.gridwithphotos.data.DataSource.topics
 import com.example.gridwithphotos.model.Topic
 import com.example.gridwithphotos.ui.theme.GridWithPhotosTheme
 
@@ -46,10 +54,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GridApp() {
-    GridCard(
-        topic = Topic(R.string.architecture, 58, R.drawable.architecture)
-    )
+fun GridApp(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier
+            .padding(8.dp),
+        verticalArrangement=Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        items(topics) { topic ->
+            GridCard(
+                topic = topic
+            )
+        }
+    }
 }
 
 @Composable
