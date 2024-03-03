837eacc879fdc32e9cc6144d48b7873577c9b0a2
Konrad <55025128+Konrad6666@users.noreply.github.com>
2024-03-03 14:44:02 +0100

    converted dessert to viewmodel

--- Unit4/basic-android-kotlin-compose-training-dessert-clicker-starter/app/src/main/java/com/example/dessertclicker/ui/DessertUiState.kt
+++ Unit4/basic-android-kotlin-compose-training-dessert-clicker-starter/app/src/main/java/com/example/dessertclicker/ui/DessertUiState.kt
@@ -2,5 +2,7 @@ package com.example.dessertclicker.ui
 
 data class DessertUiState(
     val totalRevenue: Int = 0,
-    val dessertsSold: Int = 0
+    val dessertsSold: Int = 0,
+    val currentDessertPrice: Int = 0,
+    val currentDessertImageId: Int = 0
 )
